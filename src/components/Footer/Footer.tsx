2abf522cd31fa42510442782db6716255777c18d
n1kford <n1kf0rd@mail.ru>
2022-10-01 21:57:47 +0600

    fastfix

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.scss";
 
 const Footer: FC = () => {
   return (
-    <div className={styles.Footer} data-aos="fade-up">
+    <div className={styles.Footer}>
       <div className={styles.wrapper}>
         <h4>myfirsthns</h4>
         <p>by n1kford.</p>
